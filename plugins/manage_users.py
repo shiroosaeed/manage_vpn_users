8b0293547ebeabefc7d22b0deca2b57cdecb5387
saeedshiroo <shiroosaeed@gmail.com>
2023-05-15 09:28:42 +0330

    v1

--- plugins/manage_users.py
+++ plugins/manage_users.py
@@ -3,10 +3,8 @@ from pyrogram.types import ReplyKeyboardMarkup
 import pyrostep
 from helpers.cisco_manage import Cisco
 config={
- 'username':'',
- 'password':'',
-
-
+ "username":"",
+ "password":"",
 }
    
 
@@ -28,20 +26,20 @@ async def add_account(c,m):
     await pyrostep.register_next_step(m.from_user.id, set_user_name)
 
 async def set_user_name(c,m):
-    config.username=m.text
+    config['username']=m.text
     await m.reply('enter password')
     await pyrostep.register_next_step(m.from_user.id, set_password)
 
 async def set_password(c,m):
     await m.reply('select vpn core')
-    config.password=m.text
+    config['password']=m.text
     await pyrostep.register_next_step(m.from_user.id, set_vpn_core)
 
 async def set_vpn_core(c,m):
     if m.text=='cisco':
         cisco_vpn=Cisco()
 
-        cisco_vpn.create_user(config.username,config.password)
+        res=cisco_vpn.create_user(config.username,config.password)
         await m.reply('user created')
 
         
